2402eb8cc07f9a174ce113de897fb4555ff16221
Ryujelly <wima90@gmail.com>
2021-10-20 09:18:01 +0900

    JPA활용 Thymeleaf와 RestAPI

---
+++ src/main/java/jpastudy/jpashop/service/MemberService.java
@@ -0,0 +1,32 @@
+package jpastudy.jpashop.service;
+
+import jpastudy.jpashop.domain.Member;
+import jpastudy.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor    //final로 선언된 변수를 인자로 하는 생성자
+public class MemberService {
+    //@Autowired
+    private final MemberRepository memberRepository;
+
+    // 회원 중복 검증
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    // 회원가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+}
