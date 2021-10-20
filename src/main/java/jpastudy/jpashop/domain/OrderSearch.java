7511a3f4291b8026d6b6b4ae5fe28267b72153f0
Ryujelly <wima90@gmail.com>
2021-10-20 11:45:47 +0900

    OrderServuce, Repository 추가

---
+++ src/main/java/jpastudy/jpashop/domain/OrderSearch.java
@@ -0,0 +1,13 @@
+package jpastudy.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+    //회원이름
+    private String meberName;
+
+    //주문상태(ORDER, CANCEL)
+    private OrderStatus orderStatus;
+}
