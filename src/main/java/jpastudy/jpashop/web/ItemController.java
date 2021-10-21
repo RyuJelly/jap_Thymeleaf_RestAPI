ee923ea0f678045d560bb12e717353b2c9005e4d
Ryujelly <wima90@gmail.com>
2021-10-21 09:30:00 +0900

    Item 목록처리

--- src/main/java/jpastudy/jpashop/web/ItemController.java
+++ src/main/java/jpastudy/jpashop/web/ItemController.java
@@ -1,6 +1,7 @@
 package jpastudy.jpashop.web;
 
 import jpastudy.jpashop.domain.item.Book;
+import jpastudy.jpashop.domain.item.Item;
 import jpastudy.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,5 +40,11 @@ public class ItemController {
         return "redirect:/";
     }
 
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
 
 }
